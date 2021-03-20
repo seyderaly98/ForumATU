94cef18f8ab3c5123e90d3b01bcad92ce3c93fe7
Almat <seyderaly98@gmail.com>
2021-03-20 20:52:32 +0600

    Страница HomeController.cs => Views => Index.cshtml подкльючена к базе

--- ForumATU/Controllers/HomeController.cs
+++ ForumATU/Controllers/HomeController.cs
@@ -6,16 +6,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ForumATU.Models;
+using ForumATU.Models.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace ForumATU.Controllers
 {
     public class HomeController : Controller
     {
+        private UserManager<User> UserManager { get; }
+        private ForumContext _db { get; }
+
+        public HomeController(UserManager<User> userManager, ForumContext db)
+        {
+            UserManager = userManager;
+            _db = db;
+        }
+
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            var itemEvents = _db.ItemEvents.Include(i=>i.Topics).ThenInclude(t =>t.Author).ToList();
+            return View(itemEvents);
         }
 
         
