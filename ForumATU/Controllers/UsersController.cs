13d53d2a2663dde552b42911936b81ff3ab8723e
Almat <seyderaly98@gmail.com>
2021-04-02 17:52:57 +0600

    Закомментирован UserController для ознакомления

--- ForumATU/Controllers/UsersController.cs
+++ ForumATU/Controllers/UsersController.cs
@@ -11,87 +11,76 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ForumATU.Controllers
 {
+    [Authorize]
     public class UsersController : Controller
     {
+        #region поле
         UserManager<User> _userManager { get; set; }
         ForumContext _db { get; set; }
+        
+        #endregion
+        
+        #region конструкторы
         
         public UsersController(UserManager<User> userManager, ForumContext db)
         {
             _userManager = userManager;
             _db = db;
         }
-
+        
+        #endregion
+        
+        #region Actions
 
         // GET
-        [Authorize]
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var userId = _userManager.GetUserId(User);
-            var user = await _db.Users.FirstOrDefaultAsync(u => u.Id == userId);
-            return View(new UserData(user));
+            var userId = _userManager.GetUserId(User);                                                             // Получаем Id авторизованного пользователя 
+            var user = await _db.Users.FirstOrDefaultAsync(u => u.Id == userId);                                    // Получаем экземпляр авторизованного пользовтеля
+            return View(new UserData(user));                                                                       // Вернем виде UserData
         }
-                  
-        [Authorize]
+        
+        
         [HttpPost]
         [ActionName("Index")]
         public async Task<IActionResult> UserEdit(UserData model)
         {
-            try
-            {
-                /// Валидация request
-                if (ModelState.IsValid)                                                                  
+                if (ModelState.IsValid)                                                                                 // Валидация request (Проверка, являются ли какие-либо значения состояния модели в этом modelstatedictionary недопустимыми или не проверенными.)
                 {       
-                    // Получаем Id авторизованного пользователя 
-                    var userId = _userManager.GetUserId(User);       
-                    // Получаем экземпляр самого пользовтеля
-                    var user = await _db.Users.FirstOrDefaultAsync(u => u.Id == userId);                 
-                    if (user != null) // user не должно быть null (страховка)                            
+                    var userId = _userManager.GetUserId(User);                                                     // Получаем Id авторизованного пользователя 
+                    var user = await _db.Users.FirstOrDefaultAsync(u => u.Id == userId);                            // Получаем экземпляр авторизованного пользовтеля
+                    if (user != null)                                                                                   // user не должно быть null (страховка)                            
                     {         
-                        // Редактируем даные пользователя. В данном случае используем метод Edit класса User
-                        user.Edit(model);    
-                        /// Обновляем Db
-                        await _userManager.UpdateAsync(user);
-                        /// Переадресуем на Get Index
-                        return RedirectToAction("Index");                                                       
+                        user.Edit(model);                                                                               // Редактируем даные пользователя. В данном случае используем метод Edit класса User
+                        await _userManager.UpdateAsync(user);                                                           // Обновляем Db
+                        return RedirectToAction("Index");                                                               // Переадресуем на Get Index
                     }    
-                    /// Если Пользовтель не найден (такого не должно быть) возвращаем статус кода 404
-                    return NotFound();                                                                   
+                    return NotFound();                                                                                  // Если Пользовтель не найден (такого не должно быть) возвращаем статус кода 404
                 }    
-                /// Если Request (model) не проходит валидацию, тогда возвращаем ошибку валидации
-                return View(model);                                                                      
+                return View(model);                                                                                     // Если Request (model) не проходит валидацию, тогда возвращаем ошибку валидации
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
 
-
-        [Authorize]
+        
         [HttpPost]
         public async Task<IActionResult> UpdateStatus(string status)
         {
             try
             {
-                if (!string.IsNullOrEmpty(status))
-                {
-                    var userId = _userManager.GetUserId(User);
-                    var user = await _db.Users.FirstOrDefaultAsync(u => u.Id == userId);
-                    if (user != null)
-                    {
-                        user.Status = status;
-                        await _userManager.UpdateAsync(user);
-                        return Json(true);
-                    }
-                }
-                return Json(false);
+                if (string.IsNullOrEmpty(status)) return Json(false);                                               // Проверка: Если status пустой, вернуть false. Пользователь получает сообщение (Статус не сохранен).. Также на уровне view имеется валидация 
+                var userId = _userManager.GetUserId(User);                                                         // Получить Id авторизованного  пользователя   
+                var user = await _db.Users.FirstOrDefaultAsync(u => u.Id == userId);                                // Получить экземпляр (данные) авторизованного пользователя 
+                user.Status = status;                                                                                   // Присвоить новый статус пользователю  
+                await _userManager.UpdateAsync(user);                                                                   // Обновить статус
+                return Json(true);                                                                                  // Вернуть true. Статус пользователя обновляется на новой                                                                                 
             }
             catch (Exception ex)
             {
-                return Json(ex.Message);
-            }
+                return Json(ex.Message);                                                                                // В случае ошибки вернем message ошибки. 99.9% это не пригодится если не трогать корень 
+            }                                                                                                           // Пользователю откроется 'alert' чтобы смог обновить страницу и сразу попробовать еще раз изменить статус 
         }
+        
+        #endregion
+
     }
 }
